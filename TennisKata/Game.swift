a0d503c1f204a724bdbfbb35ca62f0ceede5e7a1
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-29 11:03:43 +0800

    Refactored getScore() to look up tennis score from a Dictionary

--- TennisKata/Game.swift
+++ TennisKata/Game.swift
@@ -1,16 +1,20 @@
 import Foundation
 
 class Game {
+    let tennisScores = [
+        0: "Love",
+        1: "Fifteen",
+        2: "Thirty"
+    ]
+
     var player1Score = 0
 
     func getScore() -> String {
-        if player1Score == 1 {
-            return "Fifteen-Love"
-        } else if player1Score == 2 {
-            return "Thirty-Love"
-        } else {
-            return "Love-Love"
+        if let tennisScore = tennisScores[player1Score] {
+            return "\(tennisScore)-Love"
         }
+
+        return ""
     }
 
     func scorePlayer1() {
