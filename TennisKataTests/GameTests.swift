401dd5ea1f71a5cce62a936a523276f00853f53c
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-29 10:49:09 +0800

    Initial GameTests

---
+++ TennisKataTests/GameTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+class GameTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
